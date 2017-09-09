16d640a5ca975e8f3f90304b60c2f1c4ed0df1b5
Wittawat Saikliang <jokerce31@gmail.com>
2017-09-10 01:03:30 +0700

    update install ansible commends

--- bootstrap-mgmt.sh
+++ bootstrap-mgmt.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 # install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get update
 apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
+
+ 
+# http://docs.ansible.com/ansible/latest/intro_installation.html#latest-releases-via-apt-ubuntu
+# $ sudo apt-get update
+# $ sudo apt-get install software-properties-common
+# $ sudo apt-add-repository ppa:ansible/ansible
+# $ sudo apt-get update
+# $ sudo apt-get install ansible
 
 EOL
