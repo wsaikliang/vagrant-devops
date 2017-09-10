a8fd7c265d19d383f7e4a8b98db254c56e34d090
Wittawat Saikliang <jokerce31@gmail.com>
2017-09-11 00:41:07 +0700

    add provision jenkins file

---
+++ install-jenkins.sh
@@ -0,0 +1,4 @@
+#https://wiki.jenkins.io/display/JENKINS/Installing+Jenkins+on+Ubuntu
+#http://www.inanzzz.com/index.php/post/eh2c/setting-up-jenkins-on-ubuntu-with-vagrant-and-accessing-it-on-internet-with-ngrok
+apt-get -y install openjdk-8-jre
+apt-get -y install openjdk-8-jdk
