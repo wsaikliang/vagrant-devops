7d5b1c04973ee44f9852d9da3dec75bb47b8db3d
wittawat <jokerce31@gmail.com>
2018-10-26 01:56:20 +0700

    install-sdkman

---
+++ install-sdkman.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get -y install zip
+apt-get -y install unzip
+zip -v
+unzip -v
+
+#How to run this as ubuntu user?
+#curl -s "https://get.sdkman.io" | bash
+#source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+#export PATH=$PATH:"~/.sdkman/candidates/maven/current/bin"
+#. ~/.profile
